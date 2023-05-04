d9444bb861eacc602c44ba92031bf25939c34f01
meursault00 <akibengolea@icloud.com>
2023-05-04 09:15:49 -0300

    pop up y reorganizacion home view

--- src/components/GrifoBox.vue
+++ src/components/GrifoBox.vue
@@ -1,30 +1,32 @@
-<!-- <template>
-  <v-card class="toggle-card">
-    <v-row align="center">
-      <v-col>
-        <v-card-title class="d-flex align-center">
-          <span class="mr-2">Grifo</span>
-          <v-switch v-model="toggleValue" aria-label="Toggle" color="primary" />
-        </v-card-title>
-      </v-col>
-    </v-row>
+// GrifoBox
+
+<template>
+  <v-card :class="{'bg-on': toggleValue, 'bg-off': !toggleValue}" class="toggle-card" @click="toggleCard">
+    <v-toolbar :rounded="true" class="rounded-toolbar" transparent>
+      <v-toolbar-title class="text--white font-weight-bold text-h4 mb-0">Grifo</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon @click="toggleValue = !toggleValue; if (!toggleValue) resetValues()" 
+          :class="{'primary': toggleValue}">
+        <v-icon>{{ toggleValue ? 'mdi-power' : 'mdi-power-standby' }}</v-icon>
+      </v-btn>
+    </v-toolbar>
     <v-card-text>
       <v-row align="center">
         <v-col>
-          <v-text-field v-model="numericValue" type="number" label="Cantidad" />
+          <v-text-field v-model="numericValue" type="number" label="Cantidad"  />
         </v-col>
         <v-col>
-          <v-select v-model="selectedItem" :items="dropdownItems" label="Unidad" />
+          <v-select v-model="selectedItem" :items="dropdownItems" label="Unidad"  />
         </v-col>
         <v-col>
-          <v-btn color="primary" @click="handleButtonClick">Dispensar</v-btn>
+          <v-btn :disabled="!toggleValue" color="primary" @click="handleButtonClick();">Dispensar</v-btn>
         </v-col>
       </v-row>
     </v-card-text>
   </v-card>
-</template> -->
+</template>
 
-<!-- <script>
+<script>
 export default {
   name: 'GrifoBox',
   data() {
@@ -32,108 +34,22 @@ export default {
       toggleValue: false,
       numericValue: 0,
       selectedItem: null,
-      dropdownItems: ['ml', 'cl', 'dl','l'],
-    };
-  },
-  methods: {
-    handleButtonClick() {
-      // handle button click event here
-    },
-  },
-};
-</script> -->
-
-<style scoped>
-.toggle-card {
-  cursor: pointer;
-  padding: 16px;
-  border-radius: 10px;
-  border: 1px solid #ccc;
-  background-color: #EECC66;
-  transition: all .2s ease-in-out;
-  max-height: 200px;
-  max-width: 400px;
-}
-
-.toggle-card:hover {
-  box-shadow: 0 4px 10px rgba(0, 0, 0, .2);
-}
-</style>
-*/
-
-<template>
-  <v-card :class="{'bg-on': isOn, 'bg-off': !isOn}" class="toggle-card" @click="toggleCard">
-    <v-toolbar  :rounded="true" class="rounded-toolbar" transparent>
-
-      
-      <v-toolbar-title class="text--white font-weight-bold text-h4 mb-0">Aspiradora</v-toolbar-title>
-      
-      <v-spacer></v-spacer>
-      
-      <v-btn icon @click="isOn = !isOn" :class="{'primary': isOn}">
-        <v-icon>{{ isOn ? 'mdi-power' : 'mdi-power-standby' }}</v-icon>
-      </v-btn>
-
-    </v-toolbar>
-
-    <v-col cols="12">
-        <v-subheader>Ubicación</v-subheader>
-    </v-col>
-      
-    <v-row no-gutters class="button-row">
-  <v-col cols="auto">
-    <v-btn icon class="back-button" @click="goBack">
-      <v-icon>mdi-arrow-left</v-icon>
-    </v-btn>
-  </v-col>
-  <v-col cols="auto" class="ml-auto">
-    <v-row no-gutters>
-      <v-col cols="auto">
-        <v-btn :color="isOn ? (switchValue ? 'primary' : 'offcolor') : 'offcolor'" dark :block="true" @click.stop="toggleAspiradora" class="text-right small-button">{{ aspiradoraText }}</v-btn>
-      </v-col>
-      <v-col cols="auto">
-        <v-btn :color="isOn ? (switchValue ? 'offcolor' : 'primary') : 'offcolor'" dark :block="true" @click.stop="toggleTrapeadora" class="text-right small-button small-right-button">{{ trapeadoraText }}</v-btn>
-      </v-col>
-    </v-row>
-  </v-col>
-</v-row>
-
-
-  </v-card>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      isOn: false,
-      switchValue: true,
-      buttonColor: 'primary',
-      titleColor: 'secondary',
-      aspiradoraText: 'Grifo',
-      trapeadoraText: 'Trapear',
+      dropdownItems: ['litros', 'galones', 'mililitros', 'onces'],
+      disabledFields: false
     }
   },
   methods: {
     toggleCard() {
-      /* IR A ASPIRADORA */ 
+      this.disabledFields = !this.disabledFields;
     },
-    toggleAspiradora() {
-      if(this.isOn){// si esta prendida la aspiradora
-        this.trapearIsOn = !this.trapearIsOn;
-        if(this.buttonColor !== 'primary'){
-          this.buttonColor = this.buttonColor === 'primary' ? 'offcolor' : 'primary';
-          this.switchValue = !this.switchValue; // toggle switch value
-        }
-      }
+    handleButtonClick() {
+      console.log(`Dispensing ${this.numericValue} ${this.selectedItem}...`);
+      resetValues();
     },
-    toggleTrapeadora() {
-      if (this.isOn) {// si esta prendida la aspiradora
-        if(this.buttonColor !== 'offcolor'){ 
-          this.switchValue = !this.switchValue; // toggle switch value
-          this.buttonColor = this.switchValue ? 'primary' : 'offcolor'; // update button color
-        }
-      }
+    resetValues() {
+      this.numericValue = 0;
+      this.selectedItem = null;
+      this.disabledFields = false;
     }
   }
 }
@@ -144,7 +60,6 @@ export default {
   cursor: pointer;
   padding: 16px;  
   border-radius: 10px;
-  border: 1px solid #ccc;
   background-color: #EECC66;
   transition: all .2s ease-in-out;
   max-height: 200px;
@@ -168,14 +83,4 @@ export default {
 .bg-off {
   background-color: #8C783A;
 }
-
-.small-button {
-  padding: 5px 8px;
-  font-size: 12px;
-  border-radius: 4px;
-}
-.v-subheader {
-  margin-top: -34px;
-}
 </style>
-
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,39 +1,47 @@
+// HomeView
+
 <script setup>
 import Box from '../components/Box.vue';
 import AspiradoraBox from '../components/AspiradoraBox.vue';
 import CortinaBox from '@/components/CortinaBox.vue';
 import GrifoBox from '@/components/GrifoBox.vue';
+import HeladeraBox from '@/components/HeladeraBox.vue';
 import HornoBox from '@/components/HornoBox.vue';
-
 </script>
 
 <template>
   <main>
     <div class="canvas">
-      <AspiradoraBox class="grid-item"></AspiradoraBox>
-      <CortinaBox class="grid-item"></CortinaBox>
-      <GrifoBox class="grid-item"></GrifoBox>
-      <HornoBox class="grid-item"></HornoBox>
+      <AspiradoraBox class="grid-item" />
+      <CortinaBox class="grid-item" />
+      <GrifoBox class="grid-item" />
+      <HeladeraBox class="grid-item" />
+      <HornoBox class="grid-item" />
+
     </div>
   </main>
 </template>
 
 <style scoped>
-
 .canvas {
   width: 95%;
   margin-top: 3%;
-  height: 2632px;
-  background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.2) 100%), rgba(233, 247, 242, 0.03);
+  height: auto;
+  background: linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0) 0%,
+      rgba(0, 0, 0, 0.2) 100%
+    ),
+    rgba(233, 247, 242, 0.03);
   border-radius: 38px;
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  grid-gap:  2%;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 2%;
   padding: 2.5%;
 }
 
-
-
-
-
+.grid-item {
+  width: 100%;
+  height: 100%;
+}
 </style>
