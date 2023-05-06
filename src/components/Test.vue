6bfc3fc5396aec83deed4a8cbb487855c5e37463
JoacoGirod <joaquingirod@gmail.com>
2023-05-05 22:29:31 -0300

    Conexion a la API

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,72 +1,95 @@
 <script setup>
-import { reactive } from 'vue';
-import { ref, computed, defineProps, defineEmits } from 'vue'
+import { onMounted } from '@vue/runtime-core';
 import { useAppStore } from '@/store/app';
-
+import { RoomApi } from '@/API/room.js';
+import { ref, computed, defineProps, defineEmits, reactive } from 'vue'
 const store = useAppStore();
 
-const tempRoomName = ref("");
-const tempRoutineName = ref("");
-const tempDeviceName = ref("");
-const tempDeviceType = ref("");
-const tempDeviceRoom = ref("");
 
+const loading = ref(true);
+
+const selectedRoomName = ref("");
+const newRoomName = ref("");
+
+function clearVar(){
+    selectedRoomName.value = "";
+}
+
+onMounted(async () => {             // cuando se monta la pagina pido los datos
+    try {
+    // pido el update de los datos
+    await store.getAllRoomsAPI();
+    loading.value = false;          // una vez updateados los uso
+    } catch (error) {
+    console.error(error);
+    }
+});
 
 </script>
 
 <template>
     <main>
+        <h1>TESTTTTTTTTTTTTTt</h1>
+        <v-card class="pa-8">
 
-        <!-- ROOMS -->
-        <v-btn @click="store.startDummy">START DUMMY</v-btn>
-        <v-card>
-            <v-card-title>Add a Room!</v-card-title>
-            <v-card-text>Current Rooms {{ store.getRooms }}</v-card-text>
-            <v-text-field label="Room Name" v-model="tempRoomName"></v-text-field>
-            <v-btn @click="store.addRoom(`${tempRoomName}`)">Add</v-btn>
+            <v-card  v-if="loading.value">
+                <v-card-title>
+                    <v-card-text>Loading...</v-card-text>
+                </v-card-title>
+            </v-card>
+
+            <v-card v-else flat>
+                <v-card class="mb-4 pr-8" color="lightersecondary"  elevation="0">
+                    <v-card-title>
+                        <v-card-text class="text-h4 font-weight-bold text-uppercase ">Rooms </v-card-text>
+                    </v-card-title>
+                    <v-card class="ml-10" v-if="store.rooms.length !=0" flat>
+                    <v-list rounded  bg-color="secondary" >
+                        <v-list-item  v-for="roomName in store.getRoomNames" >
+                            {{ roomName }}
+                            <v-divider></v-divider>
+                        </v-list-item>
+                    </v-list>
+                    </v-card>
+                        <v-card-text>{{ store.rooms }}</v-card-text>
+                </v-card>
+                <v-card class="mb-4" color="lightersecondary" elevation="0">
+                    <v-card-title>
+                        <v-card-text class="text-h5 font-weight-bold ">Create a Room </v-card-text>
+                    </v-card-title>
+                    <v-text-field class="pa-8" label="Room Name"></v-text-field>
+                    <v-btn elevation="0" color="secondary" class="ml-8 mb-8"> CONFIRM </v-btn>
+                </v-card>
+                <v-card class="mb-4" color="lightersecondary" elevation="0">
+                    <v-card-title>
+                        <v-card-text class="text-h5 font-weight-bold">Update a Room :</v-card-text>
+                    </v-card-title>
+                    <v-select
+                    class="pl-8 pt-8 pr-8"
+                    label="Select the Room"
+                    :items="store.getRoomNames"
+                    v-model="selectedRoomName"
+                    ></v-select>
+                    <v-text-field class="pa-8" label="New Name" v-model="newRoomName"></v-text-field>
+                    <v-btn elevation="0" color="secondary" class="ml-8 mb-8"  @click="() => { store.updateARoomByName(selectedRoomName, newRoomName); clearVar(); }" > CONFIRM </v-btn>
+                </v-card>
+                <v-card class="mb-4" color="lightersecondary" elevation="0">
+                    <v-card-title>
+                        <v-card-text class="text-h5 font-weight-bold">Delete a Room :</v-card-text>
+                    </v-card-title>
+                    <v-select
+                    class="pl-8 pt-8 pr-8"
+                    label="Select the Room"
+                    :items="store.getRoomNames"
+                    v-model="selectedRoomName"
+                    ></v-select>
+                    <v-btn elevation="0" color="secondary" class="ml-8 mb-8"  @click="() => { store.deleteARoomByName(selectedRoomName); clearVar(); }" > CONFIRM </v-btn>
+                </v-card>
+            </v-card>
         </v-card>
-        <v-divider></v-divider>
-
-        <!-- ROUTINES -->
-        <v-card>
-            <v-card-title>Add a Routine!</v-card-title>
-            <v-card-text>Current Routines {{ store.getRoutines }}</v-card-text>
-            <v-text-field label="Routine Name" v-model="tempRoutineName"></v-text-field>
-            <v-btn @click="store.addRoutine(`${tempRoutineName}`)">Add</v-btn>
-        </v-card>
-        <v-divider></v-divider>
-
-        <!-- DEVICES -->
-        <v-card>
-            <v-card-title>Add a Device!</v-card-title>
-            <v-card-text>Current Devices {{ store.devices }}</v-card-text>
-            <v-text-field label="Device Name" v-model="tempDeviceName"></v-text-field>
-            <v-list>
-                <v-list-item>Supported Types : </v-list-item>
-                <v-list-item v-for="deviceType in store.getSupportedDevices">
-                    {{ deviceType }}
-                </v-list-item>
-            </v-list>
-            <v-text-field label="Device Type" v-model="tempDeviceType"></v-text-field>
-            <v-list>
-                <v-list-item>Available Rooms : </v-list-item>
-                <v-list-item v-for="room in store.getRooms">
-                    {{ room.name }}
-                </v-list-item>
-            </v-list>
-            <v-text-field label="Device Room" v-model="tempDeviceRoom"></v-text-field>
-            <v-btn @click="store.addDevice(`${tempDeviceName}`,`${tempDeviceType}`,`${tempDeviceRoom}`)">Add</v-btn>
-            <v-select
-            label="Devices"
-            :items="store.getDevices"
-            ></v-select>
-
-        </v-card>
-        <v-divider></v-divider>
-
-
     </main>
 </template>
+
 
 <style scoped>
 main{
--- src/store/app.js
+++ src/store/app.js
@@ -1,20 +1,273 @@
 // Utilities
 import { defineStore } from 'pinia'
 
+
+import { removeItemFromArray } from '@/Lib/lib.js'
+import { getIdByName } from '@/Lib/lib.js'
+
+
+import { DevicesApi } from '@/API/devices';
+import { RoomApi } from '@/API/room.js';
+
+/*
+Prototipos :
+  Rooms :
+    rooms[] getAllRooms();
+    rooms[] getAllRoomsAPI();                     [ASYNC]   // esta funcion no deberia fuera del Mount
+    room{}  getARoom(id);
+    room{}  getARoomByName(name);
+    room{}  createARoom(roomName);                [ASYNC]
+    boolean updateARoom(id, newname);             [ASYNC]
+    boolean updateARoomByName(oldname, newname);  [ASYNC]
+    boolean deleteARoom(id):                      [ASYNC]
+    boolean deleteARoomByName(name, id):          [ASYNC]
+
+
+*/
+
+
 export const useAppStore = defineStore('app', {
   state: () => ({
-    roomId : 0,
-    routineId : 0,
-    deviceId : 0,
-
     rooms : [],
     routines : [],
     devices : [],
-
-    supportedDevices : ['Sprinkler/Tap','Vacuum Cleaner', 'Oven', 'Fridge', 'Curtain']
+    // vacuum cleaner, oven, fridge, curtain, tap/sprinkler
+    supportedDevices : [{
+      id: "eu0v2xgprrhhg41g",
+      name: "Blinds"
+      },{
+      id: "im77xxyulpegfmv8",
+      name: "Oven"
+      },{
+      id: "ofglvd9gqx8yfl3l",
+      name: "Vacuum"
+      },{
+      id: "rnizejqr2di0okho",
+      name: "Refrigerator"
+      },{
+      id: "dbrlsh7o5sn8ur4i",
+      name: "Faucet"
+    }]
   }),
+
+
+
   actions :{
-    startDummy(){
+    /* -------------------------------------------------- ROOMS -------------------------------------------------- */
+    getAllRooms(){
+      return this.rooms;
+    },
+    async getAllRoomsAPI() {
+      try {
+        const result = await RoomApi.getAll();
+        this.rooms = result;
+        return result;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    getARoom(id){
+      return this.rooms.find( room => room.id == id);
+    },
+    getARoomByName(name){
+      var roomId = getIdByName(this.rooms, name);
+      return this.getARoom(roomId);
+    },
+
+
+
+    async createARoom(roomName){
+      try {
+        var roomObj = {
+          name : roomName
+        }
+        var result = await RoomApi.add(roomObj);
+        this.rooms.push(result);
+        return result;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+
+
+
+    async updateARoom(id, newname){
+      try {
+        var roomObj = {
+          id : id,
+          name : newname
+        }
+        var result = await RoomApi.modify(roomObj);
+        this.rooms.find( room => room.id == id).name = newname;
+        return result;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async updateARoomByName(oldname, newname){
+      try {
+        var roomId = getIdByName(this.rooms, oldname);
+        return this.updateARoom(roomId, newname);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+
+
+
+    async deleteARoom(id){
+      try {
+        var result = await RoomApi.remove(id);
+        removeItemFromArray(this.rooms, id);
+        return result;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async deleteARoomByName(name){
+      try {
+        var roomId = getIdByName(this.rooms, name);
+        return this.deleteARoom(roomId);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+
+
+
+    /* -------------------------------------------------- DEVICES -------------------------------------------------- */
+    getAllDevices(){
+      return this.devices;
+    },
+    async getAllDevicesAPI() {
+      try {
+        const result = await DevicesApi.getAll();
+        this.devices = result;
+        return result;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    getADevice(id){
+      return this.devices.find( devices => devices.id == id);
+    },
+    getADeviceByName(name){
+      var deviceId = getIdByName(this.devices, name);
+      return this.getADevice(deviceId);
+    },
+
+
+
+    async createADevice(deviceName, type){
+      try {
+        var typeId = getIdByName(this.supportedDevices, type);
+        var deviceObj = {
+          type : {
+            id : typeId
+          },
+          name : deviceName
+        }
+        var result = await DevicesApi.add(deviceObj);
+        this.devices.push(result);
+        return result;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+
+
+
+    async updateADevice(id, newname){
+      try {
+        var deviceObj = {
+          id : id,
+          name : newname,
+          meta : {}
+        }
+        var result = await DevicesApi.modify(deviceObj);
+        this.devices.find( device => device.id == id).name = newname;
+        return result;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async updateADeviceByName(oldname, newname){
+      try {
+        var deviceId = getIdByName(this.devices, oldname);
+        this.updateADevice(deviceId, newname);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+
+
+
+    async deleteADevice(id){
+      try {
+        var result = await DevicesApi.remove(id);
+        removeItemFromArray(this.devices, id);
+        return result;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async deleteADeviceByName(name){
+      try {
+        var deviceId = getIdByName(this.devices, name);
+        return this.deleteADevice(deviceId);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+
+
+    /* -------------------------------------------------- ROUTINES -------------------------------------------------- */
+    addRoutine(routineName){
+      this.routines.push({
+        id : this.routineId,
+        name : routineName,
+        actions : []
+      });
+      this.routineId++;
+    },
+    addAction(routineName, changes){
+      /*
+          esto va a ser un find segun routine name, y le agrega una accion
+          como platnear las acciones, podria ser una funcion eterna donde la mayoria es null
+          podria ser un objeto y se buscan matches pero medio feo
+          podria recibir el type y facilitar el destructuring, y asume que vienen en orden, lo que es controlable
+      */
+    },
+  },
+  getters :{
+    getRoomNames(){
+      var arr = [];
+      for ( let i = 0; i < this.rooms.length; i++ ){
+        arr.push( this.rooms[i].name );
+      }
+      return arr;
+    },
+    getDevicesNames(){
+      var arr = [];
+      for ( let i = 0; i < this.devices.length; i++ ){
+        arr.push( this.devices[i].name );
+      }
+      return arr;
+    },
+    getSupportedDevicesNames(){
+      var arr = [];
+      for ( let i = 0; i < this.supportedDevices.length; i++ ){
+        arr.push( this.supportedDevices[i].name );
+      }
+      return arr;
+    },
+  }
+})
+
+
+
+
+/* startDummy(){
       this.addRoom('Kitchen');
       this.addDevice('Main Tap','Sprinkler/Tap','Kitchen');
       this.addDevice('Super Oven','Oven','Kitchen');
@@ -33,124 +286,4 @@ export const useAppStore = defineStore('app', {
       this.addRoutine('Cooking!');
       this.addRoutine('Relaxing!');
 
-    },
-    addRoom(roomName){
-      this.rooms.push({
-        id : this.roomId,
-        name : roomName,
-        devices : []
-      });
-      this.roomId++;
-    },
-    addRoutine(routineName){
-      this.routines.push({
-        id : this.routineId,
-        name : routineName,
-        actions : []
-      });
-      this.routineId++;
-    },
-    addAction(routineName, changes){
-      /*
-          esto va a ser un find segun routine name, y le agrega una accion
-          como platnear las acciones, podria ser una funcion eterna donde la mayoria es null
-          podria ser un objeto y se buscan matches pero medio feo
-          podria recibir el type y facilitar el destructuring, y asume que vienen en orden, lo que es controlable
-      */
-    },
-
-    addDevice(deviceName, deviceType, deviceRoom){
-      if ( this.rooms.length == 0 )
-        return;
-      /* Lo agrego a los devices de un room */
-      this.rooms.find( room => room.name == deviceRoom).devices.push(
-        {
-          id: this.deviceId,
-          name: deviceName,
-          deviceType : deviceType,
-        }
-      )
-      /* va a haber un case aca */
-      /*
-        Yo supongo que esta seccion se va a cambiar por objetos a los cuales les haces new
-        y utilizas sus getters y setters para obtener
-      */
-      /* Agrego el device a los devices */
-      if ( deviceType === 'Vacuum Cleaner'){
-        this.devices.push({
-          id: this.deviceId,
-          name: deviceName,
-          deviceType : deviceType,
-          icon : "mdi-vacuum",
-          room : deviceRoom,
-          state : "Off",
-          mode : "Vacuum",
-          base : deviceRoom
-          })
-      }
-      if ( deviceType === 'Sprinkler/Tap'){
-        this.devices.push({
-          id: this.deviceId,
-          name: deviceName,
-          deviceType : deviceType,
-          icon : "mdi-water",
-          room : deviceRoom,
-          state : "Off"
-          })
-      }
-      if ( deviceType === 'Curtain'){
-        this.devices.push({
-          id: this.deviceId,
-          name: deviceName,
-          deviceType : deviceType,
-          icon : "mdi-curtains-closed",
-          room : deviceRoom,
-          state : "Open",
-          position : 0,
-          })
-      }
-      if ( deviceType === 'Oven'){
-        this.devices.push({
-          id: this.deviceId,
-          name: deviceName,
-          deviceType : deviceType,
-          icon : "mdi-stove",
-          room : deviceRoom,
-          state : "Off",
-          temperature : 0,
-          source : "",
-          grill : "Off",
-          convection : "Off"
-          })
-      }
-      if ( deviceType === 'Fridge'){
-        this.devices.push({
-          id: this.deviceId,
-          name: deviceName,
-          deviceType : deviceType,
-          icon : "mdi-fridge",
-          room : deviceRoom,
-          freezerTemp : 0,
-          fridgeTemp : 0,
-          mode : 'Normal'
-          })
-      }
-      this.deviceId++;
-    }
-  },
-  getters :{
-    getRooms : (state) =>{
-      return state.rooms;
-    },
-    getRoutines : (state) =>{
-      return state.routines;
-    },
-    getDevices : (state) =>{
-      return state.devices;
-    },
-    getSupportedDevices : (state) =>{
-      return state.supportedDevices;
-    },
-
-  }
-})
+    }, */
