c3d90c3a938f513d922e44863caacc671f54b099
cijjas <cijjas@itba.edu.ar>
2023-05-13 19:12:39 -0300

    yarn add @vueuse/motion

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -4,10 +4,35 @@
 
 <template>
     <main>
-        <h1>Error 404 : Page Not Found</h1>
+        <v-card class="err" >
+
+            <v-card-title class="mt-16 ml-8 font-weight-bold text-h2 card-title">404</v-card-title>
+            <v-card-text class=" ml-8">Page not found</v-card-text>
+            <v-card-actions class="actions-style" style="height: 150px">
+                <v-btn color="primary" text to="/">Back To Home</v-btn>
+            </v-card-actions>
+
+        </v-card>
     </main>
-</template>
+</template> 
 
 <style scoped>
-
+.err{
+    background-image: url('@/components/ComponentsAssets/background-long.png'); 
+    background-position-y: -100px;
+    background-position-x: -100px;
+    height: 500px; 
+    width: 500px; 
+    margin: 100px; 
+    margin-left: 600px; 
+    border-radius: 30px
+} 
+.actions-style {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color:#ee6161;
+    
+  }
 </style>
